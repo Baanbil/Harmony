40950be70e1224665a799045812d67bf4abb6849
Baaanbil <athellobills@gmail.com>
2024-10-20 20:32:27 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function  button(){
+  const nav = document.getElementById("nav")
+  const text = document.getElementById("text")
+  const music = document.getElementById("music")
+  const audio = document.getElementById("audio")
+
+  music.style.display = "block"
+  text.style.display = "none"
+  nav.style.display = "block"
+}
